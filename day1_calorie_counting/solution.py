7f6dd28b6a46bb33bdaa8b76e2e692166fc18d0a
John Sattari <johnsattari@johns-air.lan>
2022-12-01 22:30:57 -0800

    updated solution for day1 with notes

--- day1_calorie_counting/solution.py
+++ day1_calorie_counting/solution.py
@@ -4,14 +4,19 @@
 import pathlib
 from typing import List
 
+# load input data from file
 folder_filename = "/day1_calorie_counting/input.txt"
 data = open(str(pathlib.Path().absolute()) + folder_filename).read()
+
+# split into list of ints
 split_data = data.split("\n\n")
 calories = [[int(x) for x in text.split("\n")] for text in split_data]
 
 
 def max_cals(kcals: List[List[int]]) -> int:
-
+    """
+    Part1: How many total Calories is that Elf carrying?
+    """
     max_cals = 0
 
     for num in range(0, len(kcals) - 1):
@@ -23,6 +28,9 @@ def max_cals(kcals: List[List[int]]) -> int:
 
 def top_three(kcals: List[List[int]]) -> int:
 
+    """
+    Part2: How many Calories are those (top3) Elves carrying in total?
+    """
     summed_cals = [sum(val) for val in kcals]
 
     sorted_cals = sorted(summed_cals, reverse=True)
