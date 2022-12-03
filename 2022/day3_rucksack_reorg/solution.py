2b52c243aa7b2be5c464bbfcab5cce23a2cf8f61
John Sattari <johnsattari@johns-air.lan>
2022-12-03 11:34:08 -0800

    completed solution for day 3 part 1

--- 2022/day3_rucksack_reorg/solution.py
+++ 2022/day3_rucksack_reorg/solution.py
@@ -27,6 +27,9 @@ def dupe_finder(list1: List[int], list2: List[int]) -> int:
 
 
 if __name__ == "__main__":
-    txt1, txt2 = splitter("PmmdzqPrVvPwwTWBwg")
-    nums1, nums2 = convert(txt1), convert(txt2)
-    print(dupe_finder(nums1, nums2))
+    total = 0
+    for string in data:
+        txt1, txt2 = splitter(string)
+        nums1, nums2 = convert(txt1), convert(txt2)
+        total += int(dupe_finder(nums1, nums2))
+    print(f"Part 1: {total}")
