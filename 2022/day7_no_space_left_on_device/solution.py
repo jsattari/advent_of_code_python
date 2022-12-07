ccd0adc308d173bdae7e068bf1c37a5007a7acc9
John Sattari <johnsattari@johns-air.lan>
2022-12-07 11:21:14 -0800

    added files for day 7

---
+++ 2022/day7_no_space_left_on_device/solution.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import pathlib
+
+# load input data from file
+folder_filename = "/2022/day7_no_space_left_on_device/input.txt"
+path_ = str(pathlib.Path().absolute())
+data = open(path_ + folder_filename).read()
