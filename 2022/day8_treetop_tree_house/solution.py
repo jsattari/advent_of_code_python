a5967676f53e589a9278a09146327f44bc7a38c5
John Sattari <johnsattari@johns-air.lan>
2022-12-08 15:02:23 -0800

    added files for day 8

---
+++ 2022/day8_treetop_tree_house/solution.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import pathlib
+
+# load input data from file
+folder_filename = "/2022/day8_treetop_tree_house/input.txt"
+path_ = str(pathlib.Path().absolute())
+data = open(path_ + folder_filename).read().splitlines()
