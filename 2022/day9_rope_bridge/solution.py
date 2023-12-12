e75a88435a4c0751abda3c85314920b115d8f0d8
jsattari <jsattari3@gmail.com>
2023-12-12 10:23:07 -0800

    removed 2022 folder

--- 2022/day9_rope_bridge/solution.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-
-import pathlib
-
-# load input data from file
-folder_filename = "/2022/day9_rope_bridge/input.txt"
-path_ = str(pathlib.Path().absolute())
-data = open(path_ + folder_filename).read().splitlines()
