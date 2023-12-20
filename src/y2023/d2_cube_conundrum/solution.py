7d7fd6b02b479498c85cdf83f246fef4d8534700
jsattari <jsattari3@gmail.com>
2023-12-19 23:12:20 -0800

    addes part2 solution

--- src/y2023/d2_cube_conundrum/solution.py
+++ src/y2023/d2_cube_conundrum/solution.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-from typing import List
+from typing import List, Dict
 import re
 
 import sys
@@ -45,7 +45,37 @@ def part1(dataset: str) -> int:
     return sum(total)
 
 
+def part2(dataset: str) -> int:
+
+    # pattern
+    pattern = r'(\d+)\s*(red|blue|green)'
+
+    total: int = 0
+
+    for line in dataset.split("\n"):
+        parts = line.split(";")
+
+        CUBES: Dict[str, List[int]] = {
+            "red": [],
+            "green": [],
+            "blue": []
+        }
+
+        # parse colors, then add value to CUBES
+        for part in parts:
+            matches = re.findall(pattern, part)
+
+            for val, color in matches:
+                CUBES[color].append(int(val))
+
+        # get max value of each color
+        total += max(CUBES["red"]) * max(CUBES["green"]) * max(CUBES["blue"])
+
+    return total
+
+
 if __name__ == "__main__":
     data = open(file_finder()).read()
 
-    print(part1(data))
+    # print(part1(data))
+    print(part2(data))
