88c989e0518d33b57a3c50326bf52a8409149b5d
John Sattari <jsattar3@gmail.com>
2024-12-01 17:52:24 -0800

    refactored function to use file_path_object input

--- utility/helpers.py
+++ utility/helpers.py
@@ -4,18 +4,18 @@ from pathlib import Path
 from typing import Union
 
 
-def file_finder() -> Union[Path, None, Exception]:
+def file_finder(file_path_obj: Path) -> Union[Path, None, Exception]:
     """Find input file within folder.
 
     Args:
-        None
+        file_path_obj:  File path as object
 
     Returns:
         Data contained within input file saved within folder.
     """
 
-    # find frame of caller
-    curr_dir = Path.cwd()
+    # get parent path of input file path
+    curr_dir = file_path_obj.parents[0]
 
     # create path of input.txt
     input_file_path = curr_dir / "input.txt"
