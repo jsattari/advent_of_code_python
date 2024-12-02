9a8b59263a781b366114a7e61c97fdca50d811f7
John Sattari <jsattar3@gmail.com>
2024-12-01 22:40:14 -0800

    found solution for part 1

--- src/y2024/historian_hysteria/solution.py
+++ src/y2024/historian_hysteria/solution.py
@@ -3,6 +3,7 @@
 # ruff: noqa: E402
 # mypy: disable-error-code="union-attr"
 
+from typing import List
 from pathlib import Path
 
 import sys
@@ -13,6 +14,40 @@ sys.path.append(str(curr_path))
 
 from utility.helpers import file_finder
 
+
+def part_one(dataset: str) -> int:
+    """Calculate part one solution.
+
+    Args:
+        dataset:     input data source
+
+    Returns:
+        solution
+    """
+    # create two lists to hold data
+    list_1: List[int] = []
+    list_2: List[int] = []
+
+    # loop through data string, parse
+    for line in dataset.split("\n"):
+        # split nums
+        num_left, num_right = line.split()
+
+        list_1.append(int(num_left))
+        list_2.append(int(num_right))
+
+    # sort lists
+    list_1.sort()
+    list_2.sort()
+
+    output = 0
+
+    for i in range(0, len(list_1)):
+        output += abs(list_1[i] - list_2[i])
+
+    return output
+
+
 if __name__ == "__main__":
     # current file path
     curr_file = Path(__file__)
@@ -21,4 +56,4 @@ if __name__ == "__main__":
     with file_finder(curr_file).open("r") as file:
         data = file.read()
 
-    print(data)
+    print(f"Part one: {part_one(data)}")
