8a7f2a758de0528bc4eaf824fa0146bb515d26a1
John Sattari <jsattar3@gmail.com>
2024-12-01 17:28:17 -0800

    revised function to fix notes and file path

--- utility/helpers.py
+++ utility/helpers.py
@@ -1,27 +1,24 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
+"""Function to load inputs from current folder."""
 
-import inspect
 from pathlib import Path
 from typing import Union
 
 
 def file_finder() -> Union[Path, None, Exception]:
-    """
-        find input file within folder
+    """Find input file within folder.
+
+    Args:
+        None
+
+    Returns:
+        Data contained within input file saved within folder.
     """
 
     # find frame of caller
-    frame = inspect.stack()[1]
-
-    # find get filename from frame
-    filename = frame[1]
-
-    # get absolute path
-    filepath = Path(filename).resolve()
+    curr_dir = Path.cwd()
 
     # create path of input.txt
-    input_file_path = filepath.parent / "input.txt"
+    input_file_path = curr_dir / "input.txt"
 
     if input_file_path.is_file():
         return input_file_path
